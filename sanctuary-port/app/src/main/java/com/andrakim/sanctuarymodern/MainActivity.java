64e3d31ec354dc12479524b794b99dc00cae1380
AndrewKim <girstakilop@gmail.com>
2026-08-08 00:00:16 +0500

    Sanctuary port: add modern Android activity

---
+++ sanctuary-port/app/src/main/java/com/andrakim/sanctuarymodern/MainActivity.java
@@ -0,0 +1,31 @@
+package com.andrakim.sanctuarymodern;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        if (android.os.Build.VERSION.SDK_INT >= 30) {
+            getWindow().setDecorFitsSystemWindows(false);
+            WindowInsetsController controller = getWindow().getInsetsController();
+            if (controller != null) {
+                controller.hide(WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
+                controller.setSystemBarsBehavior(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+            }
+        } else {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    0x00000004 | 0x00000002 | 0x00001000 | 0x00000400 | 0x00000200);
+        }
+
+        setContentView(new Level1View(this));
+    }
+}
